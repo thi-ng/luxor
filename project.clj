194a8871ebc1dee9ba0b40a66c32640bc01e59de
Karsten Schmidt <k@postspectacular.com>
2014-03-13 03:47:55 +0000

    begin 0.2.1-SNAPSHOT, update geom dep

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject thi.ng/luxor "0.2.0"
+(defproject thi.ng/luxor "0.2.1-SNAPSHOT"
   :description "Scene generator for LuxRender"
   :url "https://github.com/thi-ng/luxor"
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0-beta2"]
                  [thi.ng/common "0.1.2"]
-                 [thi.ng/geom "0.2.0"]])
+                 [thi.ng/geom "0.2.1-SNAPSHOT"]])
