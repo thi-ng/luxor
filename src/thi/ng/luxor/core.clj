b95864bc063eba938c619a612b64cfb94845690a
Karsten Schmidt <k@postspectacular.com>
2014-03-14 16:54:22 +0000

    refactor core ns, split out serialization & export into io ns

--- src/thi/ng/luxor/core.clj
+++ src/thi/ng/luxor/core.clj
@@ -1,20 +1,16 @@
 (ns thi.ng.luxor.core
   (:require
    [thi.ng.luxor.config :as conf]
-   [thi.ng.luxor.compiler :refer [luxvalues-typed]]
    [thi.ng.luxor.color :as col]
+   [thi.ng.luxor.io :as lio]
    [thi.ng.luxor.presets :as presets]
-   [thi.ng.luxor.version :refer [version]]
    [thi.ng.common.math.core :as m]
    [thi.ng.geom.core :as g :refer [vec3 M44]]
    [thi.ng.geom.plane :as pl]
    [thi.ng.geom.gmesh :as gmesh]
-   [thi.ng.geom.meshio :as mio]
    [clojure.java.io :as io])
   (:import
-   [java.io File ByteArrayOutputStream OutputStream]
-   [java.util.zip ZipOutputStream ZipEntry]
-   [java.util Date]))
+   [java.io ByteArrayOutputStream]))
 
 (defn kw-or-num? [x] (or (keyword? x) (number? x)))
 (defn kw-or-str? [x] (or (keyword? x) (string? x)))
@@ -28,138 +24,6 @@
 
 (defn ->radians [theta] (if conf/*degrees* (m/radians theta) theta))
 (defn ->degrees [theta] (if conf/*degrees* theta (m/degrees theta)))
-
-(defn file-mesh-stream
-  [id path]
-  (println "writing:" path)
-  (io/output-stream path))
-
-(defprotocol PAsByteArray
-  (as-byte-array [_]))
-
-(extend-protocol PAsByteArray
-  String
-  (as-byte-array [_] (.getBytes _ "UTF-8"))
-
-  ByteArrayOutputStream
-  (as-byte-array [_] (.toByteArray _)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; serialization
-
-(defn- lx-header
-  [path & comments]
-  (format "# %s\n# generated %s by luxor v%s\n%s\n"
-          path (.toString (Date.)) version
-          (if (seq comments)
-            (reduce #(str % "# " %2 "\n") "#\n# Comments:\n" comments)
-            "")))
-
-(defn- path-filename
-  [path] (.getName (File. ^String path)))
-
-(defn- include-file*
-  [path]
-  (format "Include \"%s\"\n\n" path))
-
-(defn- include-partials*
-  [partials]
-  (apply str (map include-file* partials)))
-
-(defn- inject-scene-partial
-  [partial path include?]
-  (when partial
-    (if include? (include-file* path) partial)))
-
-(defn- serialize-lxs
-  [scene base-path separate-files?]
-  (let [base-name (path-filename base-path)
-        lxs-path (str base-path ".lxs")
-        lxs (str
-             (apply lx-header lxs-path (:comments scene))
-             (include-partials* (get-in scene [:includes :headers]))
-             (:renderer scene)
-             (:accel scene)
-             (:sampler scene)
-             (:integrator scene)
-             (:volume-integrator scene)
-             (:filter scene)
-             (:film scene)
-             (:camera scene)
-             "WorldBegin\n\n"
-             (include-partials* (get-in scene [:includes :partials]))
-             (inject-scene-partial (:volumes scene) (str base-name ".lxv") separate-files?)
-             (inject-scene-partial (:materials scene) (str base-name ".lxm") separate-files?)
-             (inject-scene-partial (:geometry scene) (str base-name ".lxo") separate-files?)
-             (:lights scene)
-             "\nWorldEnd\n")]
-    {:path lxs-path :body lxs}))
-
-(defn- serialize-scene-component
-  [group path]
-  (when group
-    {:path path :body (str (lx-header path) group)}))
-
-(defn serialize-scene
-  ([scene base-path]
-     (serialize-scene scene base-path true))
-  ([scene base-path separate?]
-     (let [scene* (reduce
-                   (fn [s [k type]]
-                     (if-let [ents (k scene)]
-                       (assoc s k (luxvalues-typed scene type ents))
-                       s))
-                   (select-keys scene [:comments :includes])
-                   {:renderer :renderer
-                    :accel :accelerator
-                    :sampler :sampler
-                    :integrator :integrator
-                    :volume-integrator :volume-integrator
-                    :filter :filter
-                    :film :film
-                    :camera :camera
-                    :lights :light
-                    :materials :material
-                    :volumes :volume
-                    :geometry :shape})
-           serialized {:lxs (serialize-lxs scene* base-path separate?)}
-           serialized (if-let [collector (get-in scene [:__config :mesh-collector])]
-                        (merge serialized (collector scene))
-                        serialized)]
-       (if separate?
-         (merge
-          serialized
-          {:lxm (serialize-scene-component (:materials scene*) (str base-path ".lxm"))
-           :lxo (serialize-scene-component (:geometry scene*) (str base-path ".lxo"))
-           :lxv (serialize-scene-component (:volumes scene*) (str base-path ".lxv"))})
-         serialized))))
-
-(defn export-scene
-  ([serialized-map]
-     (export-scene
-      serialized-map
-      (fn [_ path body]
-        (println "exporting:" path)
-        (with-open [out (io/output-stream path)]
-          (let [buf ^bytes (as-byte-array body)]
-            (.write out buf 0 (alength buf)))))))
-  ([serialized-map f]
-     (doseq [[id {:keys [path body]}] serialized-map]
-       (when (and path body)
-         (f id path body)))
-     serialized-map))
-
-(defn export-archived-scene
-  [serialized-map out]
-  (with-open [out (io/output-stream out), zip (ZipOutputStream. out)]
-    (.setMethod zip ZipOutputStream/DEFLATED)
-    (doseq [[id {:keys [path body]}] serialized-map]
-      (when (and path body)
-        (let [buf ^bytes (as-byte-array body)]
-          (doto zip
-            (.putNextEntry (ZipEntry. ^String path))
-            (.write buf 0 (alength buf)))))))
-  serialized-map)
 
 (defn- make-transform-matrix
   [{:keys [scale rx ry rz axis theta translate] :or {rx 0 ry 0 rz 0} :as tx}]
@@ -627,19 +491,6 @@
     :name [:string (name id)]
     :filename [:string (or path (str (name id) ".stl"))]}))
 
-(defn lux-scene
-  []
-  (-> {:__config {:mesh-streamer file-mesh-stream}}
-      (renderer-sppm)
-      (sampler-sobol {})
-      (integrator-sppm {})
-      (filter-mitchell {})
-      (volume-integrator :multi)
-      (accelerator-qbvh {})
-      (film {})
-      (light-group "default" {})
-      (material-null "__hidden__")))
-
 (defn configure-mesh-streamer
   [scene f]
   (assoc-in scene [:__config :mesh-streamer] f))
@@ -647,11 +498,11 @@
 (defn configure-mesh-collector
   "Configures the scene to use given fn as mesh collector during
   serialization. The fn needs to accept the scene as single arg and
-  return a map of:
+  return a map of mesh-id's as keys and maps with :path, :body as values.
 
-    {mesh-id {:path export-path :body mesh}
+    {mesh-id {:path export-path :body mesh} ...}
 
-  The :body value must satisfy the PAsByteArray protocol defined in
+  The :body values must satisfy the PAsByteArray protocol defined in
   this namespace."
   [scene f]
   (assoc-in scene [:__config :mesh-collector] f))
@@ -667,3 +518,17 @@
              out)))
         (configure-mesh-collector
          (fn [_] @state)))))
+
+(defn lux-scene
+  []
+  (-> {}
+      (configure-mesh-streamer lio/file-mesh-stream)
+      (renderer-sppm)
+      (sampler-sobol {})
+      (integrator-sppm {})
+      (filter-mitchell {})
+      (volume-integrator :multi)
+      (accelerator-qbvh {})
+      (film {})
+      (light-group "default" {})
+      (material-null "__hidden__")))
